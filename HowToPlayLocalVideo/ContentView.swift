b8a3d29068d8ef955eab9d3bcee4af9311d351c2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-29 14:42:43 +0300

    init

--- HowToPlayLocalVideo/ContentView.swift
+++ HowToPlayLocalVideo/ContentView.swift
@@ -7,10 +7,23 @@
 //
 
 import SwiftUI
+import AVKit
+
+let path = Bundle.main.path(forResource: "sample", ofType: "mp4")
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        PlayerView()
+        
+//        Button("Play") {
+//
+//            let player = AVPlayer(url: URL(fileURLWithPath: path!))
+//            let videoPlayer = AVPlayerViewController()
+//
+//            videoPlayer.player = player
+//
+//        }
     }
 }
 
@@ -18,4 +31,33 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct PlayerView: UIViewRepresentable {
+  func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+  }
+  func makeUIView(context: Context) -> UIView {
+    return PlayerUIView(frame: .zero)
+  }
+}
+
+class PlayerUIView: UIView {
+  private let playerLayer = AVPlayerLayer()
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    let url = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8")!
+    let player = AVPlayer(url: url)
+    player.play()
+    
+    playerLayer.player = player
+    layer.addSublayer(playerLayer)
+  }
+  required init?(coder: NSCoder) {
+   fatalError("init(coder:) has not been implemented")
+  }
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    playerLayer.frame = bounds
+  }
 }
